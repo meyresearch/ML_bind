d9d3d5c3f8e509ba9f3adbc6f78258c1e709bd7a
victorprincipe <victorprincipe1206@gmail.com>
2021-01-14 12:47:46 +0000

    md script for tyk2 trial 2.

---
+++ productions/trial2_tyk2/md_3.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+from simtk.openmm import * 
+from simtk.openmm.app import *
+from simtk import unit
+from sys import stdout
+import sys
+
+def equilibration(prmtop_file, crd_file):
+    prmtop = AmberPrmtopFile(prmtop_file)
+    inpcrd = AmberInpcrdFile(crd_file)
+    system = prmtop.createSystem(nonbondedMethod=PME, 
+    nonbondedCutoff=1.0*unit.nanometers, constraints=HBonds, rigidWater=True, 
+    ewaldErrorTolerance=0.0005)
+    integrator = LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds, 
+    2.0*unit.femtoseconds)
+    integrator.setConstraintTolerance(0.00001)
+    platform = Platform.getPlatformByName('OpenCL')
+    simulation = Simulation(prmtop.topology, system, integrator, platform)
+    # Set the current positions
+    simulation.context.setPositions(inpcrd.positions)
+
+    # Minimization
+    print('Minimizing...')
+    simulation.minimizeEnergy()
+
+    # Generate some velocities
+    simulation.context.setVelocitiesToTemperature(300*unit.kelvin)
+    print('Equilibrating...')
+
+    # Simulation reporters
+    simulation.reporters.append(DCDReporter('equilibration.dcd', 10))
+    simulation.reporters.append(StateDataReporter('equilibration.csv', 10, step = True, potentialEnergy = True, kineticEnergy=True, temperature = True, density = True, volume = True , totalEnergy= True, separator='\t'))
+    simulation.step(3000)
+
+    # Saving data
+    positions = simulation.context.getState(getPositions=True).getPositions()
+    velocities = simulation.context.getState(getVelocities=True).getVelocities()
+    PDBFile.writeFile(simulation.topology, positions, open('equilibration.pdb', 'w'))
+    checkpoint = 'equilibration.chk'
+    simulation.saveCheckpoint(checkpoint)
+    return simulation
+
+def production(simulation):
+
+    system = simulation.system
+    # NPT for production:
+    system.addForce(MonteCarloBarostat(1*unit.atmospheres, 300*unit.kelvin, 25))
+    
+    #Removing equilibration reporters
+    simulation.reporters.pop(-1)
+    simulation.reporters.pop(-1)
+    
+    print("Running production")
+    # Simulation reporters
+    simulation.reporters.append(DCDReporter('production.dcd', 100))
+    simulation.reporters.append(StateDataReporter('production.csv', 100, step = True, potentialEnergy = True, kineticEnergy=True, temperature = True, density = True,volume=True, totalEnergy= True, separator='\t'))
+    simulation.step(50000)
+    
+    # Save final frame to PDB file
+    positions = simulation.context.getState(getPositions=True).getPositions()
+    PDBFile.writeFile(simulation.topology, positions, open('production.pdb', 'w'))
+
+
+if __name__ == '__main__':
+	simulation = equilibration(sys.argv[1], sys.argv[2])
+
+	production(simulation)
+
+
+
+
