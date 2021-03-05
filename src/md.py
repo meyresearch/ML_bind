37766eae1fcd91b6cc1d685480b923cc34bfcdaa
victorprincipe <victorprincipe1206@gmail.com>
2021-03-05 15:00:10 +0000

    NPT equilibraiton, longer equilibratoin.

--- src/md.py
+++ src/md.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from simtk.openmm import * 
+from simtk.openmm import *
 from simtk.openmm.app import *
 from parmed.openmm import *
 from simtk import unit
@@ -9,10 +9,10 @@ import sys
 def equilibration(prmtop_file, crd_file):
     prmtop = AmberPrmtopFile(prmtop_file)
     inpcrd = AmberInpcrdFile(crd_file)
-    system = prmtop.createSystem(nonbondedMethod=PME, 
-    nonbondedCutoff=1.0*unit.nanometers, constraints=HBonds, rigidWater=True, 
+    system = prmtop.createSystem(nonbondedMethod=PME,
+    nonbondedCutoff=1.0*unit.nanometers, constraints=HBonds, rigidWater=True,
     ewaldErrorTolerance=0.0005)
-    integrator = LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds, 
+    integrator = LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds,
     2.0*unit.femtoseconds)
     integrator.setConstraintTolerance(0.00001)
     platform = Platform.getPlatformByName('OpenCL')
@@ -30,9 +30,9 @@ def equilibration(prmtop_file, crd_file):
     print('Equilibrating...')
 
     # Simulation reporters
-    simulation.reporters.append(DCDReporter('equilibration.dcd', 10))
-    simulation.reporters.append(StateDataReporter('equilibration.csv', 10, step = True, potentialEnergy = True, kineticEnergy=True, temperature = True, density = True, volume = True , totalEnergy= True, separator='\t'))
-    simulation.step(5000)
+    simulation.reporters.append(DCDReporter('equilibration.dcd', 100))
+    simulation.reporters.append(StateDataReporter('equilibration.csv', 100, step = True, potentialEnergy = True, kineticEnergy=True, temperature = True, density = True, volume = True , totalEnergy= True, separator='\t'))
+    simulation.step(250000)
 
     # Saving data
     positions = simulation.context.getState(getPositions=True).getPositions()
@@ -44,21 +44,20 @@ def equilibration(prmtop_file, crd_file):
 
 def production(simulation):
 
-    system = simulation.system
     # NPT for production:
-    system.addForce(MonteCarloBarostat(1*unit.atmospheres, 300*unit.kelvin, 25))
-    
+    simulation.system.addForce(MonteCarloBarostat(1*unit.atmospheres, 300*unit.kelvin, 25))
+
     #Removing equilibration reporters
     simulation.reporters.pop(-1)
     simulation.reporters.pop(-1)
-    
+
     print("Running production")
     # Simulation reporters
     simulation.reporters.append(DCDReporter('production.dcd', 100))
     simulation.reporters.append(StateDataReporter('production.csv', 100, step = True, potentialEnergy = True, kineticEnergy=True, temperature = True, density = True,volume=True, totalEnergy= True, separator='\t'))
     simulation.reporters.append(MdcrdReporter('production.mdcrd', 100))
-    simulation.step(2500000)
-    
+    simulation.step(3000000)
+
     # Save final frame to PDB file
     positions = simulation.context.getState(getPositions=True).getPositions()
     PDBFile.writeFile(simulation.topology, positions, open('production.pdb', 'w'))
@@ -68,7 +67,3 @@ if __name__ == '__main__':
     deviceindex = sys.argv[3]
     simulation = equilibration(sys.argv[1], sys.argv[2])
     production(simulation)
-
-
-
-
