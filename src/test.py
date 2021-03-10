94783ce72ae05e3b96d0a70335558be8398e977b
victorprincipe <victorprincipe1206@gmail.com>
2021-03-10 12:55:19 +0000

    Test changes.

--- src/test.py
+++ src/test.py
@@ -32,11 +32,12 @@ print('Minimizing...')
 simulation.minimizeEnergy()
 
 simulation.context.setVelocitiesToTemperature(300*unit.kelvin)
-print('Equilibrating...')
-simulation.step(100)
 
 simulation.reporters.append(app.DCDReporter('equilibration.dcd', 100))
 simulation.reporters.append(app.StateDataReporter('equilibration.csv', 100, step = True, potentialEnergy = True, kineticEnergy=True, temperature = True, density = True, volume = True , totalEnergy= True, separator='\t'))
+
+print('Equilibrating...')
+simulation.step(100)
 
 system.addForce(mm.MonteCarloBarostat(1*unit.atmospheres, 300*unit.kelvin, 25))
 
