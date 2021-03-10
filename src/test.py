55c161e40cf276c6be9ea9c0ac3ce85f9ad23330
victorprincipe <victorprincipe1206@gmail.com>
2021-03-10 13:30:25 +0000

    Test changes.

--- src/test.py
+++ src/test.py
@@ -38,7 +38,7 @@ simulation.context.setVelocitiesToTemperature(300*unit.kelvin)
 print('Equilibrating...')
 simulation.step(100)
 
-simulation(system.addForce(mm.MonteCarloBarostat(1*unit.atmospheres, 300*unit.kelvin, 25)))
+simulation.system.addForce(mm.MonteCarloBarostat(1*unit.atmospheres, 300*unit.kelvin, 25))
 
 
 simulation.reporters.append(app.DCDReporter('trajectory.dcd', 100))
