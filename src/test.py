95682625f4cb6d03813fa10410f273890a6274a3
victorprincipe <victorprincipe1206@gmail.com>
2021-03-10 11:59:08 +0000

    Changes.

--- src/test.py
+++ src/test.py
@@ -21,7 +21,7 @@ integrator = mm.LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds,
     2.0*unit.femtoseconds)
 integrator.setConstraintTolerance(0.00001)
 system.addForce(mm.MonteCarloBarostat(1*unit.atmospheres, 300*unit.kelvin, 25))
-platform = app.getPlatformByName('OpenCL')
+platform = mm.Platform.getPlatformByName('OpenCL')
 properties = {'OpenCLPrecision': 'mixed', 'OpenCLDeviceIndex': str(deviceindex)}
 simulation = app.Simulation(prmtop.topology, system, integrator, platform,
     properties)
