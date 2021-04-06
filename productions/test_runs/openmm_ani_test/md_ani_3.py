f50992a2963cabd94dbb953032c01657c2281cc0
victorprincipe <victoprincipe1206@gmail.com>
2021-04-06 12:09:58 +0100

    Added updated scripts.

--- productions/test_runs/openmm_ani_test/md_ani_3.py
+++ productions/test_runs/openmm_ani_test/md_ani_3.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from simtk.openmm import *
 from simtk.openmm.app import *
-#from parmed.openmm import *
+from parmed.openmm import *
 from simtk import unit
 from sys import stdout
 import sys
@@ -36,7 +36,7 @@ def equilibration(prmtop_file, crd_file):
     simulation.reporters.append(DCDReporter('equilibration.dcd', 100))
     simulation.reporters.append(StateDataReporter('equilibration.csv', 100, step = True, potentialEnergy = True,
         kineticEnergy=True, temperature = True, density = True, volume = True , totalEnergy= True, separator='\t'))
-    simulation.step(10000)
+    simulation.step(250000)
 
     # Saving data
     positions = simulation.context.getState(getPositions=True).getPositions()
@@ -60,7 +60,7 @@ def production(prmtop_file, crd_file, positions, velocities):
     ml_system.addForce(MonteCarloBarostat(1*unit.atmospheres, 300*unit.kelvin, 25))
 
     integrator = LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds,
-    2.0*unit.femtoseconds)
+    1.0*unit.femtoseconds)
     integrator.setConstraintTolerance(0.00001)
     platform = Platform.getPlatformByName('OpenCL')
     properties = {'OpenCLPrecision': 'mixed'}
@@ -75,12 +75,12 @@ def production(prmtop_file, crd_file, positions, velocities):
     print("Running production...")
 
     # Simulation reporters
-    simulation.reporters.append(DCDReporter('production.dcd', 100))
+    #simulation.reporters.append(DCDReporter('production.dcd', 100))
     simulation.reporters.append(StateDataReporter('production.csv', 100, step = True, potentialEnergy = True,
         kineticEnergy=True, temperature = True, density = True,volume=True, totalEnergy= True, separator='\t'))
     simulation.reporters.append(MdcrdReporter('production.mdcrd', 100))
 
-    simulation.step(3000)
+    simulation.step(6000000)
 
     # Save final frame to PDB file
     positions = simulation.context.getState(getPositions=True).getPositions()
